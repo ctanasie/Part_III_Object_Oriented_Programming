c68e473ac81f4ce8e14951f18151543ebd8d6db1
Cosmin Tanasie <cosmin.tanasie@outlook.com>
2017-12-21 06:57:48 +0200

    Defining static field data.

---
+++ Chapter_5_Encapsulation/StaticDataAndMembers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Static Data *****\n");
+
+            SavingsAccount s1 = new SavingsAccount(50);
+            SavingsAccount s2 = new SavingsAccount(100);
+            SavingsAccount s3 = new SavingsAccount(10000.75);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter_5_Encapsulation/StaticDataAndMembers/SavingsAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    // A simple savings account class.
+    class SavingsAccount
+    {
+        // Instance-level data.
+        public double currBalance;
+
+        // A static point of data.
+        public static double currInterestRate = 0.04;
+
+        public SavingsAccount(double balance)
+        {
+            currBalance = balance;
+        }
+    }
+}
