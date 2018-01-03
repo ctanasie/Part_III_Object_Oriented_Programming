ab62c6d416bd9d54b605d22dadd15a97dcc4d197
Cosmin Tanasie <cosmin.tanasie@outlook.com>
2018-01-03 21:24:14 +0200

    Defining static constructors.

--- Chapter_5_Encapsulation/StaticDataAndMembers/Program.cs
+++ Chapter_5_Encapsulation/StaticDataAndMembers/Program.cs
@@ -12,16 +12,20 @@ namespace StaticDataAndMembers
         {
             Console.WriteLine("***** Fun with Static Data *****\n");
 
+            // Make an account.
             SavingsAccount s1 = new SavingsAccount(50);
-            SavingsAccount s2 = new SavingsAccount(100);
 
             // Print the current interest rate.
             Console.WriteLine($"Interest Rate is: {SavingsAccount.GetInterestRate()}");
 
-            // Make a new object, this does NOT 'reset' the interest rate.
-            SavingsAccount s3 = new SavingsAccount(10000.75);
+            // Try to change the interest rate via property.
+            SavingsAccount.SetInterestRate(0.08);
 
-            Console.WriteLine($"Interest rate is: {SavingsAccount.GetInterestRate()}");
+            // Make a second account.
+            SavingsAccount s2 = new SavingsAccount(100);
+
+            // Should print 0.08 ... right?
+            Console.WriteLine($"Interest Rate is: {SavingsAccount.GetInterestRate()}");
 
             Console.ReadLine();
         }
--- Chapter_5_Encapsulation/StaticDataAndMembers/SavingsAccount.cs
+++ Chapter_5_Encapsulation/StaticDataAndMembers/SavingsAccount.cs
@@ -6,18 +6,22 @@ using System.Threading.Tasks;
 
 namespace StaticDataAndMembers
 {
-    // A simple savings account class.
     class SavingsAccount
     {
-        // Instance-level data.
         public double currBalance;
+        public static double currInterestRate;
 
-        // A static point of data.
-        public static double currInterestRate = 0.04;
-
+        // Notice that our constructor is setting the static currInterestRate value.
         public SavingsAccount(double balance)
         {
             currBalance = balance;
+        }
+
+        // A static constructor!
+        static SavingsAccount()
+        {
+            Console.WriteLine("In static ctor!");
+            currInterestRate = 0.04;
         }
 
         // Static members to get/set interest rate.
